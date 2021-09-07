24c7d53f420f81406aab8d5b58d7dfba8321fc09
ameetmund <42780136+ameetmund@users.noreply.github.com>
2021-09-08 05:07:33 +0530

    New project

--- automation.py
+++ automation.py
@@ -2,4 +2,8 @@ from selenium import webdriver
 
 chrome_browser = webdriver.Chrome('./chromedriver')
 
-chrome_browser
+chrome_browser.maximize_window()
+chrome_browser.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
+
+print('Selenium Easy' in chrome_browser.title)
+
